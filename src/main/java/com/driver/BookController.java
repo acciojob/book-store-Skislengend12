4fbfb528e4117cc8ec227fa4bac687838f412fdf
Shubham Khetre <shubhamek21@gmail.com>
2023-01-28 00:03:33 +0530

    First Commit

--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -3,10 +3,10 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("books")
@@ -16,8 +16,45 @@ public class BookController {
 
     // One example controller, make the rest by yourself
     @PostMapping("/create-book")
-    public ResponseEntity createBook(@RequestBody Book book){
+    public ResponseEntity<Book> createBook(@RequestBody Book book){
         Book newbook = bookService.createBook(book);
         return new ResponseEntity<>(newbook, HttpStatus.CREATED);
     }
+
+    @GetMapping("get-book-by-id/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable("id") String id){
+        Book book = bookService.findBookById(id);
+        return new ResponseEntity<>(book, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("get-all-books")
+    public ResponseEntity<List<Book>> findAll(){
+        List<Book> temp = bookService.findAllBooks();
+        return new ResponseEntity<>(temp, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("get-books-by-author")
+    public ResponseEntity<List<Book>> findBooksByAuthor(@PathParam("author") String author){
+        List<Book> books = bookService.findBooksByAuthor(author);
+        return new ResponseEntity<>(books, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("get-books-by-genre")
+    public ResponseEntity<List<Book>> findBooksByGenre(@PathParam("genre") String genre){
+        List<Book> books = bookService.findBooksByAuthor(genre);
+        return new ResponseEntity<>(books, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("delete-book-by-id/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable("id") String id){
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>("Book removed successfully", HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("delete-all-books")
+    public ResponseEntity<String> deleteAll(){
+        bookService.deleteAllBooks();
+        return new ResponseEntity<>("All books deleted successfully", HttpStatus.ACCEPTED);
+    }
+
 }
--- src/main/java/com/driver/BookRepository.java
+++ src/main/java/com/driver/BookRepository.java
@@ -5,37 +5,55 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepository {
-
+    int id = 1;
+    HashMap<Integer, Book> db = new HashMap<>();
     public BookRepository(){
-        
+
     }
 
     public Book save(Book book){
-        return null;
+        book.setId(id);
+        db.put(id, book);
+        id++;
+        return book;
     }
 
     public Book findBookById(int id){
-
-        return null;
+        return db.get(id);
     }
 
     public List<Book> findAll(){
-        return null;
+        return new ArrayList<>(db.values());
     }
 
     public void deleteBookById(int id){
-        return;
+        if(!db.containsKey(id))
+            return;
+        db.remove(id);
     }
 
     public void deleteAll(){
-        return;
+        db.clear();
     }
 
     public List<Book> findBooksByAuthor(String author){
-        return null;
+        List<Book> ans = new ArrayList<>();
+        for(int id : db.keySet()){
+            if(db.get(id).getAuthor().equals(author)){
+                ans.add(db.get(id));
+            }
+        }
+        return ans;
     }
 
     public List<Book> findBooksByGenre(String genre){
-        return null;
+
+        List<Book> ans = new ArrayList<>();
+        for(int id : db.keySet()){
+            if(db.get(id).getGenre().equals(genre)){
+                ans.add(db.get(id));
+            }
+        }
+        return ans;
     }
 }
